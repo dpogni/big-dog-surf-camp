1d5bb775cac59cedde9a8f11e836a5eea2e809cf
Dean Pogni <dpogni@gmail.com>
2018-05-15 22:29:04 -0700

    inital commit (7 years too late)

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="Bay Area Surf Camp, Marin, Stinson Beach, Bolinas, Proof Lab, Surf, Camp, Big Dog, Ian Glover">
+  	 	<meta name="description" content="Northern Califoria Surf Camp">
+  	 	<meta name="copyright" content="(c) Big Dog!">
+  		<meta name="author" content="deanpogni.com	">
+  		<meta name="email" content="bigdogsurfcamp@gmail.com">
+  		<meta name="Charset" content="UTF-8">
+  		<meta name="Distribution" content="Local">
+  		<meta name="Rating" content="General">
+  		<meta name="Robots" content="INDEX,FOLLOW">
+  		<meta name="Revisit-after" content="7 Day">
+		<title>Big Dog Surf Camp - <?php echo date("Y"); ?></title>
+
+		<link rel="stylesheet" href="stylesheets/default.css" media="screen" />
+	</head>
+<body>
+
+<?php include 'includes/header.html' ?>
+
+<div class="thecontainer">
+	<div class="maincontent clearfix">
+			<iframe style="display:block;margin:0 auto; text-align:center;" src="http://player.vimeo.com/video/98771622?title=0&amp;portrait=0" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p style="text-align:center;"><a href="http://vimeo.com/98771622">Big Dog Surf Camp</a> from <a href="http://vimeo.com/user5591658">Perry Gershkow</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+
+		<div style="width:350px;float:left;">
+
+		<h3>Beginner &amp; Intermediate Surf Lessons <br>Ages 5 and Up</h3>
+
+		<p>Big Dog Surf Camp is the perfect summer experience for surfers or surfers-to-be. The summer camp is perfect for any young eager kids living in Marin County and San Francisco. We will provide the boards &amp; wetsuits. Week long and single sessions are available. We will be surfing in Marin. Students must know how to swim and be ready to have a fun time.</p>
+
+		<p>Campers should be dropped off at <a href="http://www.prooflab.com/" target="_blank"> Proof Lab</a> (254 Shoreline Hwy, Mill Valley) and will be shuttled to the beach. The camp runs from 9am to 4pm.</p>
+
+  	<hr/>
+	<h3>New summer 2018 dates</h3>
+	<ul class="info ul">
+		<li>June 11 — 15</li>
+		<li>June 18 — 22</li>
+		<li><del>June 25 — 29</del> (Full)</li>
+		<li>July 2, 3, 5, 6 (not on 4th)</li>
+		<li>July 9 — 13</li>
+		<li><del>July 16 — 20</del> (Full)</li>
+		<li>July 23 — 27</li>
+		<li>July 30 — Aug 3</li>
+		<li>Aug 6 — 10</li>
+		<li>Aug 13 — 17</li>
+		<li>Aug 20 — 24</li>
+		<li>Aug 27 — 31</li>
+	</ul>
+
+	<span class="banner"><a href="lessons.html" target="_parent">Ask about our private lessons &amp; parties.</a></span>
+</div>
+
+
+		<div style="width: 300px; float: right;">
+
+			<iframe src="http://player.vimeo.com/video/70013062?title=0&amp;byline=0&amp;portrait=0&amp;color=bbbcbd" width="300" height="169" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p style="text-align:center;"><a href="http://vimeo.com/70013062">The Sunnydale Kids</a> from <a href="http://vimeo.com/rippleeffectsurf">Ripple Effect</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+
+		<img src="images/side2.jpg" alt="Big Dog Surf Camp" id="contentPhoto" style="margin-top:0;" />
+		<span class="small right" style="margin: -20px 24px 0 0">Coach Robin, Coach Jay, Coach Big Dog, & Coach Deezle</span>
+		<span class="banner3 right" style="margin-right:24px;"><a href="http://brandhabit.typepad.com/brandhabit/2008/08/surf-lessons.html" target="_blank">Check out a review of Big Dog Surf Camp &rarr;</a></span>
+		<span class="banner3 right" style="margin-right:24px;"><a href="https://vimeo.com/bigdogsurfcamp" target="_blank">Also check out our videos on Vimeo &rarr;</a></span>
+
+
+		</div>
+
+	</div>
+</div>
+
+<div id="footer">
+<span>&copy; Big Dog Surf Camp</span>&nbsp;|&nbsp;
+<a href="mailto:bigdogsurfcamp@gmail.com" target="_blank">bigdogsurfcamp@gmail.com</a>&nbsp;|&nbsp;
+Phone: (415) 518-3805&nbsp;|&nbsp;
+<a href='http://maps.google.com/maps?f=d&hl=en&geocode=&saddr=&daddr=PROOF+LAB&sll=40.979898,-105.556641&sspn=33.964475,58.974609&ie=UTF8&z=16'>
+Get Directions
+</a>
+
+
+</div>
+<!--<div class="madeby">created by <a href="http://www.deanpogni.com" target="_blank">Dean Pogni</div>-->
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-4353674-1");
+pageTracker._trackPageview();
+</script>
+</body>
+</html>
---
+++ sign-up/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Bay Area Surf Camp, Marin, Stinson Beach, Bolinas, Proof Lab, Surf, Camp, Big Dog, Ian Glover">
+<meta name="description" content="Northern Califoria Surf Camp">
+<meta name="copyright" content="(c) Big Dog!">
+<meta name="author" content="deanpogni.com	">
+<meta name="email" content="dpogni@gmail.com">
+<meta name="Charset" content="UTF-8">
+<meta name="Distribution" content="Local">
+<meta name="Rating" content="General">
+<meta name="Robots" content="INDEX,FOLLOW">
+<meta name="Revisit-after" content="7 Day">
+<title>Big Dog Surf Camp - Sign Up</title>
+<link rel="stylesheet" href="/stylesheets/default.css" media="screen" />
+</head>
+<body id="signup">
+
+<?php include '../includes/header.html' ?>
+
+<div class="thecontainer">
+	<div class="maincontent clearfix">
+
+		<div style="width:318px;float:left;">
+		<h3>Sign Up</h3>
+		<p>
+			Please print and fill out the forms attached. Follow the directions and send a check and both filled out pages to the address shown.</p>
+		<p class="clear"><em>We will send you a conformation email as soon as we receive it.</em> Feel free to call or email with any further question and we’ll see you soon!</p>
+
+		<p><span class="banner"><strong>Weekly camps are $550 and day of drop-ins are $120 if available.</strong></span></p>
+
+		<br class="clear"/><br/>
+
+		<ul id="pdfList">
+			<li>
+			<a href='/pdfs/BigDogWaiver.pdf'><img src='/images/pdf_icon.png' alt="download" />Waiver</a>
+			</li>
+			<li>
+			<a href='/pdfs/RegForm2018.pdf'><img src='/images/pdf_icon.png' alt="download" />2018 Registration Form</a></strong>
+			</li>
+		</ul>
+
+		</div>
+
+		<div style="width: 300px; float: right;">
+		<img src="/images/side3.jpg" alt="Big Dog Surf Camp" id="contentPhoto" />
+		<span class="small right" style="margin-right: 24px;">Big Dog Surf Camp | Marin, CA & North Bay Area</span>
+		</div>
+
+	</div>
+</div>
+
+<div id="footer">
+<span>&copy; Big Dog Surf Camp</span>&nbsp;|&nbsp;
+<a href="mailto:bigdogsurfcamp@gmail.com" target="_blank">bigdogsurfcamp@gmail.com</a>&nbsp;|&nbsp;
+Phone: (415) 518-3805&nbsp;|&nbsp;
+<a href='http://maps.google.com/maps?f=d&hl=en&geocode=&saddr=&daddr=PROOF+LAB&sll=40.979898,-105.556641&sspn=33.964475,58.974609&ie=UTF8&z=16'>
+Get Directions
+</a>
+</div>
+
+
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-4353674-1");
+pageTracker._trackPageview();
+</script>
+
+
+</body>
+</html>
