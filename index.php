c185090c3740d6f2d571da701c043ce99dda0a98
Dean Pogni <dpogni@gmail.com>
2020-07-09 11:55:09 -0700

    update homepage wording

--- index.php
+++ index.php
@@ -30,15 +30,11 @@
                     <h3>🏄‍♀️🏄‍♂️ Beginner &amp; Intermediate Surf&nbsp;Lessons <br>Ages 5 and Up</h3>
                     <p>Big Dog Surf Camp is the perfect summer experience for surfers or surfers-to-be. The summer camp is perfect for any young eager kids living in Marin County and San Francisco. We will provide the boards &amp; wetsuits. Week long and single sessions are available. We will be surfing in Marin. Students must know how to swim and be ready to have a fun time.</p>
                     <p>Campers should be dropped off near <a href="http://www.prooflab.com/" target="_blank"> Proof Lab</a>, at <a href="https://goo.gl/maps/ECkKRZFcW9Q9DgN47" target="_blank">240 Shoreline Hwy, Mill Valley</a>, and will be shuttled to the beach. The camp runs from 9am to 4pm.</p>
-                    <div class="banner">
-                        <p>Per CDC guidelines, <u>campers must be signed up for 3 consecutive weeks</u>. Campers can attend just 2 if they quarantine the 3rd week.</p>
-                        <p>If you have any questions, please email us at <a href="mailto:bigdogsurfcamp@gmail.com">bigdogsurfcamp@gmail.com</a></p>
-                    </div>
                     <hr />
                     <h3>Summer 2020 (sold out)</h3>
-                    <div class="banner banner--red">
-                        <p> Following CDC guidelines, we are at capacity for the summer.
-                        </p>
+                    <div class="banner">
+                        <p>Per CDC guidelines, campers must be signed up for 3 consecutive weeks. Campers may also attend just 2 consecutive sessions if they self-quarantine the 3rd week.</p>
+                        <p>If you have any questions, please email us at <a href="mailto:bigdogsurfcamp@gmail.com">bigdogsurfcamp@gmail.com</a></p>
                     </div>
                     <ul class="info ul">
                         <li><del>June 8 — 12</del> (sold out)</li>
@@ -46,15 +42,18 @@
                         <li><del>June 22 — 26</del> (sold out)</li>
                         <li><del>June 29 – July 3</del> (sold out)</li>
                         <li><del>July 6 — 10</del> (sold out)</li>
-                        <li><del>July 13 — 17</del> (sold out)</li>
-                        <li><del>July 20 — 24</del> (sold out)</li>
-                        <li><del>July 27 — 31</del> (sold out)</li>
-                        <li><del>Aug. 3 — 7</del> (sold out)</li>
-                        <li><del>Aug. 10 — 14</del> (sold out)</li>
-                        <li><del>Aug. 17 — 21</del> (sold out)</li>
-                        <li><del>Aug. 24 — 28</del> (sold out)</li>
-                        <li><del>Aug. 31 — Sept. 4</del> (sold out)</li>
+                        <li>July 13 — 17</li>
+                        <li>July 20 — 24</li>
+                        <li>July 27 — 31</li>
+                        <li>Aug. 3 — 7</li>
+                        <li>Aug. 10 — 14</li>
+                        <li>Aug. 17 — 21</li>
+                        <li>Aug. 24 — 28</li>
+                        <li>Aug. 31 — Sept. 4</li>
                     </ul>
+                    <div class="banner">
+                        <p>To sign up, please email <a href="mailto:bigdogsurfcamp@gmail.com">bigdogsurfcamp@gmail.com</a> or text us at <a href="tel:1-310-422-6161">(310) 422-6161</a>.</p>
+                    </div>
                     <!-- 	<span class="banner"><a href="lessons.html" target="_parent">Ask about our private lessons &amp; parties.</a></span> -->
                 </div>
                 <div class="col--right">
