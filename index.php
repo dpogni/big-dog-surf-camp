eea8f7f6fc7f7cace7c0a87ab01ca933606b3baf
Dean <dean.pogni@coinbase.com>
2023-05-09 17:01:40 -0700

    close session 3

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                     <ul class="info ul">
                         <li><strong>Session 1:</strong> June 12 – 15 &amp; <del>June 19 – 22</del> (sold out)</li>
                         <li><strong>Session 2:</strong> June 26 – 29 &amp; July 5 – 7 (Wed–Fri / $1050)</li>
-                        <li><strong>Session 3:</strong> July 10 – 13 &amp; July 17 – 20</li>
+                        <li><strong>Session 3:</strong> <del>July 10 – 13 &amp; July 17 – 20</del> (sold out)</li>
                         <li><strong>Session 4:</strong> July 24 – 27 &amp; July 31 – Aug 3</li>
                         <li><strong>Session 5:</strong> Aug 7 – 10 &amp; Aug 14 – 17</li>
                         <li><strong>Session 6:</strong> Aug 21 – 24 &amp; Aug 28 – 31</li>
