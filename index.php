1923956df510bc40cbf86dfe1f4c4e1df0669abf
Dean <dean.pogni@coinbase.com>
2023-06-06 11:14:04 -0700

    sell out week one

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                         <p>Sessions may be modified due to changing CDC and Marin County guidelines.</p>
                     </section>
                     <ul class="info ul">
-                        <li><strong>Session 1:</strong> June 12 – 15 &amp; <del>June 19 – 22</del> (sold out)</li>
+                        <li><strong>Session 1:</strong> <del>June 12 – 15 &amp; June 19 – 22</del> (sold out)</li>
                         <li><strong>Session 2:</strong> June 26 – 29 &amp; July 5 – 7 (Wed–Fri / $1050)</li>
                         <li><strong>Session 3:</strong> <del>July 10 – 13 &amp; July 17 – 20</del> (sold out)</li>
                         <li><strong>Session 4:</strong> July 24 – 27 &amp; July 31 – Aug 3</li>
